9282a8dcf14c4e1debe8f43926487a132245abaf
osmancikjan <j.osmancik@gmail.com>
2016-12-11 20:10:29 +0100

    Done tasks 1,2,3,4,5; 6 is missing

--- Skript_Dotazy.sql
+++ Skript_Dotazy.sql
@@ -56,18 +56,46 @@ EXCEPT
 (SELECT cID FROM Clenove 
 	EXCEPT 
 	SELECT cID FROM Statistiky)
-/*4;1;;*/
-
-/*4;2;;*/
-
-/*4;3;;*/
-
-/*4;4;;*/
-
-/*5;1;;*/
-/*5;2;;*/
-/*5;3;;*/
-/*5;4;;*/
-
+/*4;1;2;Vypíše týmy, které mají nadprùmìrný poèet obdržených gólù*/
+SELECT t.nazev, u.obdrzene_goly FROM Tymy t
+	JOIN Umisteni u ON u.kod = t.kod
+	GROUP BY t.nazev, u.obdrzene_goly
+	HAVING u.obdrzene_goly > (SELECT AVG(obdrzene_goly) FROM Umisteni)
+/*4;2;3;Vypíše hráèe, kteøí mají nadprùmìrný poèet trestných minut*/
+SELECT c.cID, c.jmeno, c.prijimeni, s.trestne_minuty, (SELECT AVG(trestne_minuty) FROM Statistiky) AS prumer FROM Clenove c
+	JOIN Statistiky s ON s.cID = c.cID
+	GROUP BY c.cID, c.jmeno, c.prijimeni, s.trestne_minuty
+	HAVING s.trestne_minuty > (SELECT AVG(trestne_minuty) FROM Statistiky)
+/*4;3;1;Vypíše hráèe s maximálním poètem trestných minut*/
+SELECT c.cID, c.jmeno, c.prijimeni, s.trestne_minuty, (SELECT MAX(trestne_minuty) FROM Statistiky) AS maximum FROM Clenove c
+	JOIN Statistiky s ON s.cID = c.cID
+	GROUP BY c.cID, c.jmeno, c.prijimeni, s.trestne_minuty
+	HAVING s.trestne_minuty >= (SELECT MAX(trestne_minuty) FROM Statistiky)
+/*4;4;5;Vypíše hráèe, kteøí mají souèet gólù a asistencí > 4*/
+SELECT c.cID, c.jmeno, c.prijimeni, SUM(goly+asistence) AS soucet_asistenci_a_golu FROM Clenove c
+	JOIN Statistiky s ON s.cID = c.cID
+	GROUP BY c.cID, c.jmeno, c.prijimeni
+	HAVING SUM(goly+asistence) > 4
+/*5;1;6;Vypíše PSÈ a Název mìsta, kterému pøísluší dané PSÈ. S užitím JOIN*/
+SELECT DISTINCT a.psc, m.mesto FROM Mesto m
+	JOIN Adresa a on a.psc = m.psc
+		--GROUP BY a.psc, m.mesto
+/*5;2;6;Vypíše PSÈ a Název mìsta, kterému pøísluší dané PSÈ. S užitím IN*/
+SELECT m.psc, m.mesto FROM Mesto m WHERE m.psc IN (SELECT a.psc FROM Adresa a)
+/*5;3;6;Zobrazí aktuálnì odehrané zápasy pøehlednì s výsledkem a eliminuje zápasy, které ještì hrány nebyly*/
+SELECT r.zID, t.nazev, v.domaci_goly, v.hoste_goly, t1.nazev FROM Rozpis r 
+LEFT JOIN Vysledky v ON v.zID = r.zID
+LEFT JOIN Tymy t ON t.kod = r.domaci
+LEFT JOIN Tymy t1 ON t1.kod = r.hoste
+GROUP BY r.zID, t.nazev, v.domaci_goly, v.hoste_goly, t1.nazev 
+HAVING SUM(CASE WHEN domaci_goly IS NULL AND hoste_goly IS NULL then 0 else 1 end)>0
+/*5;4;4;Vypíše neodehrané zápasy.*/
+SELECT r.zID, t.nazev, v.domaci_goly, v.hoste_goly, t1.nazev FROM Rozpis r 
+LEFT JOIN Vysledky v ON v.zID = r.zID
+LEFT JOIN Tymy t ON t.kod = r.domaci
+LEFT JOIN Tymy t1 ON t1.kod = r.hoste
+WHERE v.domaci_goly IS NULL AND v.hoste_goly IS NULL
+GROUP BY r.zID, t.nazev, v.domaci_goly, v.hoste_goly, t1.nazev 
 /*6;1;;*/
+
 /*6;2;;*/
